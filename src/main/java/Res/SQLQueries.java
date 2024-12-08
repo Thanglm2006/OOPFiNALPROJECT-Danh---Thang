181b2abc44c0a3edc37c979a909155c553289cde
ADMIN <sungang.20072009@gmail.com>
2024-12-08 16:07:42 +0700

    FINALPROJECTOOP DANH LAM THANG CANH

--- src/main/java/Res/SQLQueries.java
+++ src/main/java/Res/SQLQueries.java
@@ -2,12 +2,20 @@ package Res;
 import java.io.*;
 import java.sql.*;
 public class SQLQueries {
-    Connection c=null;
-    Statement st=null;
-    String s;
-    String search="select Word,Pronunciation,FilePath,Meaning,CategoryName From AudioForVocab" +"\n"+
+    private Connection c=null;
+    private Statement st=null;
+    private String s;
+    private String search="select Word,Pronunciation,FilePath,Meaning,CategoryName From AudioForVocab" +"\n"+
             " join Vocabulary on Vocabulary.WordID=AudioForVocab.WordID" +"\n"+
             " where Vocabulary.Word like '%s' or Vocabulary.Meaning like '%s';";
+    private String registerForStudent="insert into StudentAccount (Account, Password)" +"\n"+
+            " values ('%s', '%s');"+"\n"+
+            "insert into Student (StudentID,StudentName, Email, BirthDate) "+"\n"+
+            "values (%d,'%s', '%s','%s')";
+    private String registerForTeacher="insert into TeacherAccount (Account, Password)" +"\n"+
+            " values ('%s', '%s');"+"\n"+
+            "insert into Teacher (TeacherID,TeacherName, Email, BirthDate) "+"\n"+
+            "values (%d,'%s', '%s', '%s')";
     public SQLQueries() {
         FileInputStream f=null;
         ObjectInputStream ois=null;
@@ -16,6 +24,7 @@ public class SQLQueries {
              ois= new ObjectInputStream(f);
              s =(String) ois.readObject();
              this.c=DriverManager.getConnection(s);
+            st=c.createStatement();
         } catch (IOException | ClassNotFoundException | SQLException e) {
 
         } finally{
@@ -40,7 +49,6 @@ public class SQLQueries {
     public ResultSet  Search(String txt){
         ResultSet res=null;
         try {
-            st=c.createStatement();
             String text= String.format(search,txt+"%",txt+"%");
             res=st.executeQuery(text);
         } catch (SQLException e) {
@@ -48,12 +56,48 @@ public class SQLQueries {
         }
         return res;
     }
+    public boolean insertTeacher(String acc, String pass, String name, String email, String birth){
+        int id=0;
+        try {
+            ResultSet res= st.executeQuery("select top 1 TeacherID from Teacher order by TeacherID DESC");
+            while(res.next()){
+                id=res.getInt("TeacherID");
+            }
+        } catch (SQLException e) {
 
-    public static void main(String[] args) throws SQLException {
-        SQLQueries a= new SQLQueries();
-        ResultSet res=a.Search("xin chào");
-        while(res.next()){
-            System.out.println(res.getNString("Word"));
         }
+        id++;
+        String txt= String.format(registerForTeacher,acc,pass,id,name,email, birth);
+        try {
+            PreparedStatement pp= c.prepareStatement(txt);
+            pp.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    public boolean insertStudent(String acc, String pass, String name, String email, String birth){
+        int id=0;
+        try {
+            ResultSet res= st.executeQuery("select top 1 StudentID from Student order by StudentID DESC");
+            while(res.next()){
+                id=res.getInt("StudentID");
+            }
+        } catch (SQLException e) {
+
+        }
+        id++;
+        String txt= String.format(registerForStudent,acc,pass,id,name,email, birth);
+        try {
+            PreparedStatement pp= c.prepareStatement(txt);
+            pp.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) throws SQLException {
     }
 }
